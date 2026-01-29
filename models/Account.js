48ab0e6e29e4eff9e4b25c2fcedcf46fb0a6b88b
HAY <hayitaliotamurotov@gmail.com>
2026-01-29 13:31:07 +0500

    Create Account model with schema definition

---
+++ models/Account.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const accountSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  type: {
+    type: String,
+    enum: ["SOM", "HN", "SAVINGS", "BONUS"],
+    required: true
+  },
+  accountNumber: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  balance: {
+    type: Number,
+    default: 0
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+export const Account = mongoose.model("Account", accountSchema);
