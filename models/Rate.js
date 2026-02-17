5547c15847f1bd1ac156f8b9690cd9d591d10e44
HAY <hayitaliotamurotov@gmail.com>
2026-02-17 14:29:35 +0500

    Simplify Rate model export syntax

--- models/Rate.js
+++ models/Rate.js
@@ -1,16 +1,8 @@
 import mongoose from "mongoose";
 
-const rateSchema = new mongoose.Schema(
-  {
-    pair: { type: String, default: "HN/SOM" },
-    rate: { type: Number, default: 1200 },
-    updatedBy: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-    },
-  },
-  { timestamps: true }
-);
+const rateSchema = new mongoose.Schema({
+  pair: { type: String, default: "HN/SOM" },
+  rate: { type: Number, default: 1200 }
+}, { timestamps: true });
 
-const Rate = mongoose.model("Rate", rateSchema);
-export default Rate;
+export default mongoose.model("Rate", rateSchema);
