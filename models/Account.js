4894b9047e300493a8143cef576d5c7a1cf7abb5
HAY <hayitaliotamurotov@gmail.com>
2026-02-17 14:28:48 +0500

    Refactor accountSchema definition in Account.js

--- models/Account.js
+++ models/Account.js
@@ -1,29 +1,14 @@
 import mongoose from "mongoose";
 
-const accountSchema = new mongoose.Schema(
-  {
-    userId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-    type: {
-      type: String,
-      enum: ["SOM", "HN", "SAVINGS", "BONUS"],
-      required: true,
-    },
-    accountNumber: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    balance: {
-      type: Number,
-      default: 0,
-    },
+const accountSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  type: {
+    type: String,
+    enum: ["SOM", "HN", "SAVINGS", "BONUS"],
+    required: true
   },
-  { timestamps: true }
-);
+  accountNumber: { type: String, unique: true },
+  balance: { type: Number, default: 0 }
+}, { timestamps: true });
 
-const Account = mongoose.model("Account", accountSchema);
-export default Account;
+export default mongoose.model("Account", accountSchema);
