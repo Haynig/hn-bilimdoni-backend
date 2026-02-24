b03a28e6ff2dae25f92c84f76c676e1f335742c0
HAY <hayitaliotamurotov@gmail.com>
2026-02-24 11:52:21 +0500

    Refactor Transaction schema to include userId and type

--- models/Transaction.js
+++ models/Transaction.js
@@ -1,13 +1,12 @@
 import mongoose from "mongoose";
 
-const transactionSchema = new mongoose.Schema({
-  fromAccount: String,
-  toAccount: String,
-  amount: Number,
-  currency: { type: String, enum: ["HN", "SOM"] },
-  rate: Number,
-  type: String,
-  status: { type: String, default: "COMPLETED" }
-}, { timestamps: true });
+const transactionSchema = new mongoose.Schema(
+  {
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    type: { type: String, enum: ["deposit", "withdraw"], required: true },
+    amount: { type: Number, required: true }
+  },
+  { timestamps: true }
+);
 
 export default mongoose.model("Transaction", transactionSchema);
