39d686d2b78e668d4a3f026e7c04afa33a537e83
HAY <hayitaliotamurotov@gmail.com>
2026-02-24 11:50:04 +0500

    Update MongoDB connection log messages

--- db.js
+++ db.js
@@ -3,9 +3,9 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("✅ MongoDB connected");
+    console.log("MongoDB ulandi ✅");
   } catch (error) {
-    console.error("❌ MongoDB xatosi:", error.message);
+    console.error("MongoDB xatosi ❌", error.message);
     process.exit(1);
   }
 };
