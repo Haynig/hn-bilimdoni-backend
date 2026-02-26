eef47a3dcc479a09f7c6f87a26eef7336a4a3487
HAY <hayitaliotamurotov@gmail.com>
2026-02-26 15:59:17 +0500

    Update Transaction.js

--- models/Transaction.js
+++ models/Transaction.js
@@ -1,12 +1,11 @@
 import mongoose from "mongoose";
 
-const transactionSchema = new mongoose.Schema(
-  {
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-    type: { type: String, enum: ["deposit", "withdraw"], required: true },
-    amount: { type: Number, required: true }
-  },
-  { timestamps: true }
-);
+const transactionSchema = new mongoose.Schema({
+  userId: mongoose.Schema.Types.ObjectId,
+  fromType: String,
+  toType: String,
+  amount: Number,
+  type: String
+}, { timestamps: true });
 
 export default mongoose.model("Transaction", transactionSchema);
