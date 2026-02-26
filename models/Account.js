cae5f860e661d1e9450d4990bd0b1069f4fd3957
HAY <hayitaliotamurotov@gmail.com>
2026-02-26 15:58:39 +0500

    Update Account.js

--- models/Account.js
+++ models/Account.js
@@ -1,11 +1,10 @@
 import mongoose from "mongoose";
 
-const accountSchema = new mongoose.Schema(
-  {
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-    balance: { type: Number, default: 0 }
-  },
-  { timestamps: true }
-);
+const accountSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  type: { type: String, enum: ["HN","SOM","SAVINGS","BONUS"] },
+  accountNumber: { type: String, unique: true },
+  balance: { type: Number, default: 0 }
+});
 
 export default mongoose.model("Account", accountSchema);
