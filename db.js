9dd678c68783605d98399723cb69ba60e38f82f3
HAY <hayitaliotamurotov@gmail.com>
2026-02-26 15:56:40 +0500

    Update db.js

--- db.js
+++ db.js
@@ -3,9 +3,9 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB ulandi ✅");
-  } catch (error) {
-    console.error("MongoDB xatosi ❌", error.message);
+    console.log("✅ MongoDB ulandi");
+  } catch (err) {
+    console.log("❌ MongoDB xatosi:", err.message);
     process.exit(1);
   }
 };
