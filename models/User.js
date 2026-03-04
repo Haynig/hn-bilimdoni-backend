53da16097922468dd03bd0d0b9e3d3f05ac5c754
HAY <hayitaliotamurotov@gmail.com>
2026-03-04 12:52:39 +0500

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,9 +1,38 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  name: String,
-  email: { type: String, unique: true },
-  password: String
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+
+  password: {
+    type: String,
+    required: true
+  },
+
+  phone: {
+    type: String,
+    unique: true,
+    sparse: true   // 🔥 MUHIM
+  },
+
+  uzsBalance: {
+    type: Number,
+    default: 0
+  },
+
+  hnBalance: {
+    type: Number,
+    default: 0
+  },
+
+  cashbackBalance: {
+    type: Number,
+    default: 0
+  }
+
 }, { timestamps: true });
 
 export default mongoose.model("User", userSchema);
