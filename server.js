2f660bd5ac0ced475bdb499e64167a22755b15b2
HAY <hayitaliotamurotov@gmail.com>
2026-03-05 14:30:28 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,39 +1,33 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import connectDB from "./db.js";
 
+import connectDB from "./db.js";
 import userRoutes from "./routes/user.routes.js";
-import walletRoutes from "./routes/wallet.routes.js";
 
 dotenv.config();
+
 connectDB();
 
 const app = express();
 
 app.use(cors({
-  origin: [
-    "https://haynig.github.io"
-  ],
-  methods: ["GET","POST","PUT","DELETE"],
-  allowedHeaders: ["Content-Type","Authorization"],
-  credentials: true
+origin:[
+"https://haynig.github.io",
+"http://localhost:5500"
+]
 }));
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.json({ message: "HN Wallet API working 🚀" });
+app.get("/",(req,res)=>{
+res.json({message:"HN Wallet API ishlayapti"});
 });
 
-app.use("/api/user", userRoutes);
-app.use("/api/wallet", walletRoutes);
-
-app.use((req, res) => {
-  res.status(404).json({ error: "Route topilmadi" });
-});
+app.use("/api/user",userRoutes);
 
 const PORT = process.env.PORT || 10000;
-app.listen(PORT, () => {
-  console.log(`✅ Server ${PORT} portda ishlayapti`);
+
+app.listen(PORT,()=>{
+console.log("Server ishlayapti "+PORT);
 });
