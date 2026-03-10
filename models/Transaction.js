487007985def3ca82363a16d44f094fe51c0750a
HAY <hayitaliotamurotov@gmail.com>
2026-03-10 14:26:50 +0500

    Update Transaction.js

--- models/Transaction.js
+++ models/Transaction.js
@@ -1,11 +1,16 @@
 import mongoose from "mongoose";
 
 const transactionSchema = new mongoose.Schema({
-  userId: mongoose.Schema.Types.ObjectId,
-  fromType: String,
-  toType: String,
-  amount: Number,
-  type: String
-}, { timestamps: true });
 
-export default mongoose.model("Transaction", transactionSchema);
+user:{
+type:mongoose.Schema.Types.ObjectId,
+ref:"User"
+},
+
+type:String,
+
+amount:Number
+
+},{timestamps:true});
+
+export default mongoose.model("Transaction",transactionSchema);
