0b7ac4fa0b9fd76535bcf1e1bf0017e2dcb11248
HAY <hayitaliotamurotov@gmail.com>
2026-03-16 11:42:45 +0500

    Create shop.controller.js

---
+++ controllers/shop.controller.js
@@ -0,0 +1,32 @@
+import Product from "../models/Product.js";
+import Order from "../models/Order.js";
+import User from "../models/User.js";
+
+export const buyProduct = async(req,res)=>{
+
+const {productId,amount} = req.body;
+
+const product = await Product.findById(productId);
+
+const user = await User.findById(req.user.id);
+
+const total = product.priceHN * amount;
+
+if(user.hnBalance < total){
+return res.json({message:"Token yetarli emas"});
+}
+
+user.hnBalance -= total;
+
+await user.save();
+
+await Order.create({
+user:user._id,
+product:productId,
+amount,
+totalHN:total
+});
+
+res.json({message:"Sotib olindi"});
+
+};
